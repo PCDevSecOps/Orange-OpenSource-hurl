b17c129c7b177ae8c73d5bc9d41dc23dfd987401
Filipe PINTO <filipe.pinto@orange.com>
2024-02-06 16:07:43 +0100

    Update install_prerequisites_macos.sh

--- bin/install_prerequisites_macos.sh
+++ bin/install_prerequisites_macos.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
 brew install bash pkg-config squid
-curl -o curl.rb https://raw.githubusercontent.com/Homebrew/homebrew-core/5f1b24e9882a1c1effa559a0000ff03ae155560b/Formula/curl.rb
+curl -o curl.rb https://raw.githubusercontent.com/Homebrew/homebrew-core/5f1b24e9882a1c1effa559a0000ff03ae155560b/Formula/c/curl.rb
 brew install -s curl.rb
 PATH="/usr/local/opt/curl/bin/:$PATH"
 export PATH
