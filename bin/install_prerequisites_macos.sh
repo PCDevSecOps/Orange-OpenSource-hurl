bf78ecb57862cc4ffae6f255ac397748510f50e6
Filipe PINTO <filipe.pinto@orange.com>
2024-02-06 15:55:59 +0100

    Force curl 8.5.0 on macos while waiting for curl 8.6.1

--- bin/install_prerequisites_macos.sh
+++ bin/install_prerequisites_macos.sh
@@ -2,7 +2,9 @@
 set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
-brew install bash pkg-config squid
+brew install bash pkg-config squid curl@8.5.0
+PATH="/usr/local/opt/curl/bin/:$PATH"
+export PATH
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
 
